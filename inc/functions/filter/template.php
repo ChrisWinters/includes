6147829e85dc95bfba00d38fddfe1d511bae124a
Chris Winters <webmaster.skills@gmail.com>
2023-02-26 18:20:05 -0500

    Inject shortcode viewer into template path

---
+++ inc/functions/filter/template.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Public function.
+ */
+
+namespace Includes\Filter;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Inject shortcode viewer into template include path.
+ */
+function template(): void
+{
+    \add_filter(
+        'template_include',
+        function ($template) {
+            $postID = \get_the_ID();
+            $postType = \get_post_type($postID);
+
+            if ('includes' === $postType) {
+                $template = \Includes\settings('template_path').'/viewer.php';
+            }
+
+            return $template;
+        },
+        99
+    );
+}
