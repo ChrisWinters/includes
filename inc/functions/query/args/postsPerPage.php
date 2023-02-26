9728eecb0c622f0d629823d25d87a87d6c70a478
Chris Winters <webmaster.skills@gmail.com>
2023-02-26 18:12:15 -0500

    Sets posts_per_page modifier for query args

---
+++ inc/functions/query/args/postsPerPage.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Public function.
+ */
+
+namespace Includes\Query\Args;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Set the number of posts to query within an includes category.
+ *
+ * @param int   $postsPerPage Number of post to show per page.
+ * @param array $wpQuery      WordPress query object.
+ */
+function postsPerPage(int $postsPerPage, array $wpQuery): array
+{
+    unset($wpQuery['nopaging']);
+
+    return array_merge(
+        $wpQuery,
+        [
+            'posts_per_page' => (int) $postsPerPage,
+        ]
+    );
+}
