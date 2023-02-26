78a26b0228a7930db7a5ed1b3bd93c9381f44457
Chris Winters <webmaster.skills@gmail.com>
2023-02-26 18:11:50 -0500

    Sets order modifier for query args

---
+++ inc/functions/query/args/order.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Public function.
+ */
+
+namespace Includes\Query\Args;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Order WP Query results.
+ *
+ * @param string $order   Designates the ascending or descending order.
+ * @param array  $wpQuery WordPress query object.
+ */
+function order(string $order, array $wpQuery): array
+{
+    if (
+        true === in_array(
+            (string) $order,
+            [
+                'DESC',
+                'ASC',
+            ],
+            true
+        )
+    ) {
+        unset($wpQuery['order']);
+
+        $wpQuery = array_merge(
+            $wpQuery,
+            [
+                'order' => (string) strtoupper($order),
+            ]
+        );
+    }
+
+    return $wpQuery;
+}
