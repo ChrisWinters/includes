26cf5b09ad7d536a6ab67b9fff016017462f45a1
Chris Winters <webmaster.skills@gmail.com>
2023-02-26 18:23:22 -0500

    Display shortcode metabox

---
+++ inc/functions/plugin-admin/metabox/shortcode.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Public admin area function.
+ */
+
+namespace Includes\PluginAdmin\MetaBox;
+
+if (false === defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Display MetaBox for Includes shortcode.
+ *
+ * @param object $post Current post object.
+ */
+function shortcode(object $post): void
+{
+    if (true === empty($post) || true === empty($post->post_name)) {
+        return;
+    }
+
+    $dashIcon = '<span class="dashicons dashicons-visibility" style="margin-top:4px;"></span>';
+    $permalink = \get_permalink($post->ID);
+    $slugShortCode = htmlentities('[includes slug="'.$post->post_name.'"]', ENT_QUOTES);
+
+    // Display slug shortcode.
+    echo '<p><a href="'.$permalink.'" target="_blank" style="text-decoration:none;">'.$dashIcon.'</a> <input type="text" name="shortcode" value="'.$slugShortCode.'" size="28" autocomplete="off" onclick="this.focus();this.select()" /></p>';
+
+    // Display code shortcode.
+    if ((bool) 1 === \Includes\Option\setting('shortcode_code')) {
+        $codeView = ('publish' === $post->post_status) ? '?type=code' : '&type=code';
+        $codeShortcode = htmlentities('[includes code="'.$post->post_name.'"]', ENT_QUOTES);
+
+        echo '<p><a href="'.$permalink.$codeView.'" target="_blank" style="text-decoration:none;">'.$dashIcon.'</a> <input type="text" name="shortcode" value="'.$codeShortcode.'" size="28" autocomplete="off" onclick="this.focus();this.select()" /></p>';
+    }
+}
