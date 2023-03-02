16dae00f1c9eb541858b7b17d183e44bda659f31
Chris Winters <webmaster.skills@gmail.com>
2023-03-01 21:36:51 -0500

    Setup post type detection

--- inc/functions/filter/template.php
+++ inc/functions/filter/template.php
@@ -14,14 +14,31 @@ if (false === defined('ABSPATH')) {
  */
 function template(): void
 {
+    if (true === \is_blog_admin()) {
+        return;
+    }
+
+    // Only load if the requested URI is the includes post type.
+    // Cannot check for post type yet, but need to disable features.
+    if (
+        true === isset($GLOBALS['REQUEST_URI']) &&
+        false === str_contains($GLOBALS['REQUEST_URI'], 'includes')
+    ) {
+        return;
+    }
+
+    // Removes admin bar.
+    \add_filter('show_admin_bar', '__return_false');
+    \show_admin_bar(false);
+
+    // Replaces double line-breaks with paragraph elements.
+    \remove_filter('the_content', 'wpautop');
+
+    // Filters the path of the current template before including it.
     \add_filter(
         'template_include',
-        function (string $template): string {
-            if (true === \is_singular('includes')) {
-                $template = \Includes\settings('template_path').'/viewer.php';
-            }
-
-            return $template;
+        function (): string {
+            return \Includes\settings('template_path').'/viewer.php';
         },
         99
     );
