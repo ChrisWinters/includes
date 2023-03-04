3d006db54354a1b49225428efb89c2900d17395f
Chris Winters <webmaster.skills@gmail.com>
2023-03-04 14:36:28 -0500

    New setting to disable viewer and shortcode fields

--- inc/templates/settings.php
+++ inc/templates/settings.php
@@ -12,8 +12,6 @@ if (false === defined('ABSPATH')) {
 }
 ?>
 <section>
-	<h2 class="h3"><?php \esc_html_e('Optional plugin settings', 'includes'); ?></h2>
-
 <form enctype="multipart/form-data" method="post" action="<?php echo admin_url('admin-post.php'); ?>">
 	<?php
 \wp_nonce_field(
@@ -22,12 +20,23 @@ if (false === defined('ABSPATH')) {
 );
 ?>
 <input type="hidden" name="action" value="update" />
+	<h3><?php \esc_html_e('User interface features', 'includes'); ?></h3>
+	<hr />
+
 	<ul class="list-group p-0">
-		<li class="list-group-item my-3">
-			<input class="form-check-input me-1" name="shortcode_code" type="checkbox" value="1" id="shortcode"<?php checked((bool) 1, \Includes\Option\setting('shortcode_code')); ?>>
-			<label class="form-check-label fw-bold" for="shortcode">
-				<?php \esc_html_e('Enable code editor and allow the Includes shortcode to render saved code', 'includes'); ?>
-				<span class="description"><?php \esc_html_e('Only use if needed', 'includes'); ?></span>
+		<li class="list-group-item mb-3">
+			<input class="form-check-input me-1" name="disable_viewer" type="checkbox" value="1" id="viewer"<?php checked((bool) 1, \Includes\Option\setting('disable_viewer')); ?>>
+			<label class="form-check-label fw-bold" for="viewer">
+				<?php \esc_html_e('Enable shortcode viewer: Uses an empty template to view/test Includes', 'includes'); ?>
+				<span class="description"><?php \esc_html_e('Disable to use theme files (enabled by default)', 'includes'); ?></span>
+			</label>
+		</li>
+
+		<li class="list-group-item mb-3">
+			<input class="form-check-input me-1" name="shortcode_post_type" type="checkbox" value="1" id="post_type"<?php checked((bool) 1, \Includes\Option\setting('shortcode_post_type')); ?>>
+			<label class="form-check-label fw-bold" for="post_type">
+				<?php \esc_html_e('Enable includes shortcode field(s) on Includes post type view', 'includes'); ?>
+				<span class="description"><?php \esc_html_e('Helpful for all users (enabled by default)', 'includes'); ?></span>
 			</label>
 		</li>
 
@@ -35,10 +44,23 @@ if (false === defined('ABSPATH')) {
 			<input class="form-check-input me-1" name="shortcode_terms" type="checkbox" value="1" id="terms"<?php checked((bool) 1, \Includes\Option\setting('shortcode_terms')); ?>>
 			<label class="form-check-label fw-bold" for="terms">
 				<?php \esc_html_e('Enable includes shortcode field(s) on category term pages', 'includes'); ?>
-				<span class="description"><?php \esc_html_e('Helpful if you use categories', 'includes'); ?></span>
+				<span class="description"><?php \esc_html_e('Helpful if you use categories (enabled by default)', 'includes'); ?></span>
 			</label>
 		</li>
 
+		<li class="list-group-item my-3">
+			<input class="form-check-input me-1" name="shortcode_code" type="checkbox" value="1" id="shortcode"<?php checked((bool) 1, \Includes\Option\setting('shortcode_code')); ?>>
+			<label class="form-check-label fw-bold" for="shortcode">
+				<?php \esc_html_e('Enable code editor and allow the Includes shortcode to render saved code', 'includes'); ?>
+				<span class="description"><?php \esc_html_e('Only use if needed (disabled by default)', 'includes'); ?></span>
+			</label>
+		</li>
+	</ul>
+
+	<h3><?php \esc_html_e('Enhance WordPress features', 'includes'); ?></h3>
+	<hr />
+
+	<ul class="list-group p-0">
 		<li class="list-group-item mb-3">
 			<input class="form-check-input me-1" name="shortcode_posts_pages" type="checkbox" value="1" id="posts_pages"<?php checked((bool) 1, \Includes\Option\setting('shortcode_posts_pages')); ?>>
 			<label class="form-check-label fw-bold" for="posts_pages">
@@ -70,15 +92,8 @@ if (false === defined('ABSPATH')) {
 				<span class="description"><?php \esc_html_e('Only enable if needed, requires extra resources', 'includes'); ?></span>
 			</label>
 		</li>
-
-		<li class="list-group-item mb-3">
-			<input class="form-check-input me-1" name="disable_viewer" type="checkbox" value="1" id="viewer"<?php checked((bool) 1, \Includes\Option\setting('disable_viewer')); ?>>
-			<label class="form-check-label fw-bold" for="viewer">
-				<?php \esc_html_e('Disable shortcode viewer: Use theme template files to view saved shortcode data', 'includes'); ?>
-				<span class="description"><?php \esc_html_e('Enable/disable as needed', 'includes'); ?></span>
-			</label>
-		</li>
 	</ul>
+
 	<p class="submit"><input type="submit" name="submit" id="submit" class="btn btn-primary" value="<?php \esc_html_e('save settings', 'includes'); ?>"></p>
 </form>
 </section>
