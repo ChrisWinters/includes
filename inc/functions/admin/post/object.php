ae7f071ec03ffe83c8fbb0453747bdd7b9befdfd
Chris Winters <webmaster.skills@gmail.com>
2023-03-04 15:47:01 -0500

    Split default and allowed inputs

--- inc/functions/admin/post/object.php
+++ inc/functions/admin/post/object.php
@@ -14,22 +14,26 @@ if (false === defined('ABSPATH')) {
  */
 function object(): array
 {
-    $allowedInputs = [
+    // Required default inputs.
+    $defaultInputs = [
         'action' => FILTER_UNSAFE_RAW,
-        'submit' => FILTER_UNSAFE_RAW,
         '_wp_http_referer' => FILTER_UNSAFE_RAW,
         'includes_nonce' => FILTER_UNSAFE_RAW,
-        'shortcode_code' => FILTER_VALIDATE_BOOL,
-        'shortcode_terms' => FILTER_VALIDATE_BOOL,
-        'shortcode_posts_pages' => FILTER_VALIDATE_BOOL,
-        'shortcode_post_titles' => FILTER_VALIDATE_BOOL,
-        'shortcode_menus' => FILTER_VALIDATE_BOOL,
-        'shortcode_widget_titles' => FILTER_VALIDATE_BOOL,
     ];
 
+    // Combine with allowed inputs and filters from settings.
+    $allowedInputs = array_merge(
+        $defaultInputs,
+        \Includes\settings('allowed_inputs')
+    );
+
+    // Only allow inputs and filters defined in settings.
     $filteredObject = filter_input_array(INPUT_POST, $allowedInputs);
+
+    // Remove empty items from the post object.
     $postObject = array_filter($filteredObject);
 
+    // Data required or bail.
     if (
         true === empty($postObject['_wp_http_referer']) ||
         true === empty($postObject['includes_nonce'])
@@ -37,9 +41,11 @@ function object(): array
         return [];
     }
 
+    // Clean object.
     unset($postObject['_wp_http_referer']);
     unset($postObject['includes_nonce']);
 
+    // The post object should at least have an action.
     if (true === empty($postObject)) {
         return [];
     }
