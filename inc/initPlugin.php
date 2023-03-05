75f63dccc5eece2b30a6d95e68dc509d177b3fdc
Chris Winters <webmaster.skills@gmail.com>
2023-03-05 11:46:25 -0500

    Updated taxonomy and post type callbacks

--- inc/initPlugin.php
+++ inc/initPlugin.php
@@ -17,7 +17,7 @@ function initPlugin(): void
     // Register includes post type.
     \register_post_type(
         'includes',
-        \Includes\Args\postType()
+        \Includes\posttypeArgs()
     );
 
     // Register includes taxonomy.
@@ -26,7 +26,7 @@ function initPlugin(): void
         [
             'includes',
         ],
-        \Includes\Args\taxonomy()
+        \Includes\taxonomyArgs()
     );
 
     // Shortcode: [includes slug=""]
