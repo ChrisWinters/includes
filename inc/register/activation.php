10334c95b325b54121e3d2bf25d4b217a3ad8630
Chris Winters <webmaster.skills@gmail.com>
2023-03-18 12:46:08 -0400

    Setup default plugin settings

--- inc/register/activation.php
+++ inc/register/activation.php
@@ -24,6 +24,20 @@ function activation(): void
         \wp_die(\Includes\settings('security_message'));
     }
 
+    // Get activated flag.
+    $activated = \Includes\Option\setting('activated');
+
+    if (true === empty($activated)) {
+        // Set plugin defaults.
+        \Includes\Option\update(
+            [
+                'activated' => 1,
+                'shortcode_viewer' => 1,
+                'shortcode_fields' => 1,
+            ]
+        );
+    }
+
     // Flush rules for new post type and taxonomy.
     flush_rewrite_rules();
 }
