24589d3c7283e3f52e6380a2f8ca36153f7e060c
Chris Winters <webmaster.skills@gmail.com>
2023-03-18 12:46:37 -0400

    Added delete notice

--- inc/register/deactivation.php
+++ inc/register/deactivation.php
@@ -14,5 +14,19 @@ if (false === defined('ABSPATH')) {
  */
 function deactivation(): void
 {
+    global $pagenow;
+
+    if (
+        false === \is_admin() ||
+        'plugins.php' !== $pagenow ||
+        false === \current_user_can('manage_options')
+    ) {
+        \wp_die(\Includes\settings('security_message'));
+    }
+
+    // Make sure notice flag is removed.
+    \Includes\Option\delete('notice');
+
+    // Flush rules for new post type and taxonomy.
     \flush_rewrite_rules();
 }
